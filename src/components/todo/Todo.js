a1d60e5f2720960ef3d0c1d6610675d2ee9429ef
Godspower Idiakose <idiakeg@gmail.com>
2022-08-10 03:39:11 +0100

    Deleted the props from the todo file as the 'TodoTracker' and 'TodoList' components now get their props via context

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -2,18 +2,12 @@ import React from "react";
 import TodoList from "./TodoList";
 import TodoTracker from "./TodoTracker";
 
-const Todo = (props) => {
-	const { todos, handleDelete, handleChecked, isChecked, checkedTodos } = props;
+const Todo = () => {
 	return (
 		<>
 			<div className="todo-container">
-				<TodoTracker todos={todos} checkedTodos={checkedTodos} />
-				<TodoList
-					todos={todos}
-					handleDelete={handleDelete}
-					handleChecked={handleChecked}
-					isChecked={isChecked}
-				/>
+				<TodoTracker />
+				<TodoList />
 			</div>
 		</>
 	);
